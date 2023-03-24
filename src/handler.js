5829f73886e3e8a9c9af91d0b16de8d3c14189fa
KhoirurRo <rozikin2002@gmail.com>
2023-03-24 13:03:56 +0700

    optioanl

--- src/handler.js
+++ src/handler.js
@@ -63,10 +63,25 @@ const addBookHandler = (request, h) => {
 };
 
 const getAllBooksHandler = (request, h) => {
+  const { name, reading, finished } = request.query;
+  let tempbook = [];
+
+  if (name !== undefined) {
+    tempbook = books.filter((book) =>
+      book.name.toLowerCase().includes(name.toLowerCase())
+    );
+  }
+  if (reading !== undefined) {
+    tempbook = books.filter((book) => book.reading === !!Number(reading));
+  }
+  if (finished !== undefined) {
+    tempbook = books.filter((book) => book.finished === !!Number(finished));
+  }
+
   const response = h.response({
     status: 'success',
     data: {
-      books: books.map((item) => ({
+      books: tempbook.map((item) => ({
         id: item.id,
         name: item.name,
         publisher: item.publisher,
@@ -162,9 +177,32 @@ const editBookById = (request, h) => {
   return response;
 };
 
+const deleteBookById = (request, h) => {
+  const { bookId } = request.params;
+  const index = books.findIndex((item) => item.id === bookId);
+
+  if (index !== -1) {
+    books.splice(index, 1);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
 module.exports = {
   addBookHandler,
   getAllBooksHandler,
   getBooksById,
   editBookById,
+  deleteBookById,
 };
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const {
   getAllBooksHandler,
   getBooksById,
   editBookById,
+  deleteBookById,
 } = require('./handler');
 
 const routes = [
@@ -25,6 +26,11 @@ const routes = [
     method: 'PUT',
     path: '/books/{bookId}',
     handler: editBookById,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookById,
   },
 ];
 
